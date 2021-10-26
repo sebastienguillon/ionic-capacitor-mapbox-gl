5f3adefd44ea65386c9ba448838be4f4e1871428
Guillaume CARELLI <guillaume.carelli-ext@vinci-autoroutes.com>
2021-10-26 16:13:42 +0200

    Création de Data2 vide avec le plugin MapboxVa

--- src/app/data/ma.d.ts
+++ src/app/data/ma.d.ts
@@ -1,1 +1,3 @@
 declare module '@mapbox/polyline';
+declare module '@turf/nearest-point-to-line';
+declare module '@turf/point-to-line-distance';
--- src/app/data2/data2.page.ts
+++ src/app/data2/data2.page.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MapboxOptions } from 'mapbox-gl';
+import { MapFeatureType, MapLayer, MapOptions, VaLoggerLevel, VaMap, VaMapboxPlugin, VaPlacemark, VaPlacemarkPoint } from '@va/plugin-mapbox';
 
 @Component({
   selector: 'app-data2',
   templateUrl: './data2.page.html',
   styleUrls: ['./data2.page.scss'],
 })
-export class Data2Page implements OnInit {
+export class Data2Page implements OnInit, AfterViewInit {
+  @ViewChild('mapContainer') mapContainer?: ElementRef<HTMLDivElement>;
+  private vaMap?: VaMap;
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.initMap();
+    console.log(`MapPage ngAfterViewInit()`);
+  }
+
+  private initMap(): void {
+
+    const bounds: mapboxgl.LngLatBoundsLike = [
+      {
+        lat: 34.58165319547932,
+        lng: -5.202923933292027,
+      },
+      {
+        lat: 55.47611931257899,
+        lng: 9.129934806925206,
+      },
+    ];
+
+    if (this.mapContainer) {
+      VaMapboxPlugin.init({
+        accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA',
+        loggerLevel: VaLoggerLevel.Errors,
+      });
+  
+      const options: MapboxOptions = {
+        attributionControl: false,
+        pitchWithRotate: false,
+        container: this.mapContainer.nativeElement,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        preserveDrawingBuffer: true,
+        bounds: bounds,
+      };
+  
+      this.vaMap = VaMapboxPlugin.createMap(options);
+
+      if(this.vaMap) {
+        this.vaMap.ready()
+        .then(
+          () => {
+            this.vaMap?.mapResize();
+          },
+        );
+      }
+    }
   }
 
 }
