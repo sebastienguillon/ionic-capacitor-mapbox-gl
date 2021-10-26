eac7792325854bd82d51ee620c886480168a9d15
Sébastien Guillon <sebastien.guillon-ext@vinci-autoroutes.com>
2021-10-26 14:23:53 +0200

    Add icon size interpolation based on zoom leevel

--- src/app/data1/data1.page.ts
+++ src/app/data1/data1.page.ts
@@ -7,7 +7,9 @@ import {
   Map,
   MapboxOptions,
   LineLayer,
-  SymbolLayer
+  SymbolLayer,
+  GeoJSONSource,
+  EventData,
 } from 'mapbox-gl';
 import { MapService } from '../core/map.service';
 
@@ -25,13 +27,12 @@ export class Data1Page implements AfterViewInit, OnInit {
   ) { }
 
   ngOnInit() {
-    console.log(`MapPage ngOnInit()`);
-    console.log(`parisBordeauxAreas`, parisBordeauxAreas);
+    console.log(`Data1Page ngOnInit()`);
   }
 
   ngAfterViewInit() {
     this.initMap();
-    console.log(`MapPage ngAfterViewInit()`);
+    console.log(`Data1Page ngAfterViewInit()`);
   }
 
   private initMap(): void {
@@ -67,6 +68,10 @@ export class Data1Page implements AfterViewInit, OnInit {
         console.log(`Map "resize" event`);
       });
 
+      this.map.on('click', (ev) => {
+        console.log(`Map "click" event`, ev);
+      });
+
       this.map.on('error', (err) => {
         console.log(`Map "error" event`, err);
       });
@@ -75,39 +80,79 @@ export class Data1Page implements AfterViewInit, OnInit {
 
   ngOnDestroy() {
     this.map?.remove();
-    console.log(`MapPage ngOnDestroy(), map removed`);
+    console.log(`Data1Page ngOnDestroy(), map removed`);
   }
 
   addAreasLayer() {
     if (this.map) {
       const images = this.mapService.extractUniqueImagesFromFeatureCollection(parisBordeauxAreas, 'imageUrl');
-      console.log(`images`, images);
-      this.mapService.referenceAllImages(this.map, images).then(() => {
-        console.log('images referenced');
 
-        this.map?.addSource('areas-source', {
+      this.mapService.referenceAllImages(this.map, images).then(() => {
+        const sourceId = 'areas-source';
+
+        this.map?.addSource(sourceId, {
           type: 'geojson',
           data: parisBordeauxAreas,
         });
-
+        const defaultIconSize = 0.2;
         const layer: SymbolLayer = {
           id: 'areas',
           type: 'symbol',
-          source: 'areas-source',
+          source: sourceId,
           layout: {
             'icon-allow-overlap': true,
             'icon-image': ['get', 'imageUrl'],
-            'icon-size': 0.25,
-            ['icon-anchor']: [
+            ['icon-size']: [
+              'interpolate',
+              ['exponential', 1],
+              ['zoom'],
+              5,
+              defaultIconSize / 2,
+              10,
+              defaultIconSize * 2,
+            ],
+            /*'icon-size': [
+              'case',
+              ['has', 'icon-size'],
+              ['get', 'icon-size'],
+              defaultIconSize
+            ],*/
+            'icon-anchor': [
               'case',
               ['has', 'icon-anchor'],
               ['get', 'icon-anchor'],
               'center',
             ],
+            'symbol-sort-key': [
+              'case',
+              ['has', 'symbol-sort-key'],
+              ['get', 'symbol-sort-key'],
+              1
+            ]
           }
         };
 
         this.map?.addLayer(layer);
+        this.map?.on('click', layer.id, (ev: EventData) => {
+          console.log(`"click" event on layer "${layer.id}"`, ev);
+
+          const featureId = ev.features[0].properties.Id;
+          const source = this.map?.getSource(sourceId);
+          if (source) {
+            parisBordeauxAreas.features.forEach(feature => {
+              if (feature && feature.properties) {
+                if (feature.properties.Id === featureId) {
+                  feature.properties['icon-size'] = defaultIconSize * 1.5;
+                  feature.properties['symbol-sort-key'] = 37;
+                } else {
+                  feature.properties['icon-size'] = defaultIconSize;
+                  feature.properties['symbol-sort-key'] = 1;
+                }
+              }
+            });
+            (source as GeoJSONSource).setData(parisBordeauxAreas);
+          }
+        });
       });
     }
   }
